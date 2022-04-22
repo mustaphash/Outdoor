f6a224ebca58e96654768e8e9ca0440a264512b5
Mustafa Shakir <mshakir9918@gmail.com>
2022-04-22 20:39:38 +0300

    Add LandmarkTypeConfig

---
+++ src/DAL/Configs/LandmarkTypeConfig.cs
@@ -0,0 +1,25 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DAL.Configs
+{
+    public class LandmarkTypeConfig : IEntityTypeConfiguration<LandmarkType>
+    {
+        public void Configure(EntityTypeBuilder<LandmarkType> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .HasColumnName("Id");
+
+            builder.Property(x => x.Name)
+                .HasColumnName("Name")
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.Property(x => x.Height)
+                .HasColumnName("Height");
+        }
+    }
+}
