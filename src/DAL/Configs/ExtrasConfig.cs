04e7193bbee04b4d288c940f03dfe66a8ff723d1
Mustafa Shakir <mshakir9918@gmail.com>
2022-04-24 11:36:43 +0300

    Update Seeding

--- src/DAL/Configs/ExtrasConfig.cs
+++ src/DAL/Configs/ExtrasConfig.cs
@@ -24,12 +24,35 @@ namespace DAL.Configs
                 .IsRequired();
 
             builder.HasData(
-                new Extras { Id = 1, Name = "Wi-Fi" },
-                new Extras { Id = 2, Name = "Camping" },
-                new Extras { Id = 3, Name = "FishingAllowed" },
-                new Extras { Id = 4, Name = "Elrctricity" },
-                new Extras { Id = 5, Name = "OpenForTourist" },
-                new Extras { Id = 6, Name = "WalkingPaths" });
+                new Extras 
+                {
+                    Id = 1,
+                    Name = "Wi-Fi" 
+                },
+                new Extras 
+                {
+                    Id = 2,
+                    Name = "Camping"
+                },
+                new Extras 
+                { 
+                    Id = 3,
+                    Name = "FishingAllowed"
+                },
+                new Extras { 
+                    Id = 4, 
+                    Name = "Elrctricity" 
+                },
+                new Extras 
+                {
+                    Id = 5,
+                    Name = "OpenForTourist" 
+                },
+                new Extras 
+                {
+                    Id = 6,
+                    Name = "WalkingPaths" 
+                });
         }
     }
 }
