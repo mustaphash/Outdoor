6fab75bebceb8cfd50ee95e20928386dadb0d864
Mustafa Shakir <mshakir9918@gmail.com>
2022-05-09 12:32:20 +0300

    Create PlaceController

---
+++ src/Infrastructure/Services/PlaceService.cs
@@ -0,0 +1,15 @@
+﻿using DAL;
+using Infrastructure.Services.Abstract;
+
+namespace Infrastructure.Services
+{
+    public class PlaceService : IPlaceService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public PlaceService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+    }
+}
---
+++ src/OutdoorPlace/Controllers/PlaceController.cs
@@ -0,0 +1,17 @@
+﻿using Infrastructure.Services.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OutdoorPlace.Controllers
+{
+    [ApiController]
+    [Route("api/places")]
+    public class PlaceController : Controller
+    {
+        private readonly IPlaceService _placeService;
+
+        public PlaceController(IPlaceService placeService_)
+        {
+            _placeService = placeService_;
+        }
+    }
+}
