2712e344a5e0992ab6b50f44a941593f6a61503f
Mustafa Shakir <mshakir9918@gmail.com>
2022-06-16 16:23:31 +0300

    Add CreateParkModel

---
+++ src/Infrastructure/Models/CreateModels/CreateParkModel.cs
@@ -0,0 +1,66 @@
+﻿using Core.Entities;
+using Microsoft.AspNetCore.Http;
+
+namespace Infrastructure.Models.CreateModels
+{
+    public class CreateParkModel
+    {
+#pragma warning disable CS8618
+#pragma warning disable CS8625
+        public CreateParkModel()
+        {
+            Name = string.Empty;
+            Description = string.Empty;
+            Image = null;
+            Safety = string.Empty;
+        }
+#pragma warning restore CS8625
+#pragma warning restore CS8618
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int Year { get; set; }
+
+        public IFormFile Image { get; set; }
+
+        public double Longitude { get; set; }
+
+        public double Latitude { get; set; }
+
+        public int WorkingHours { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public string Safety { get; set; }
+
+        public Park ToLake()
+        {
+            byte[] fileBytes = new byte[] { };
+
+            if (Image.Length > 0)
+            {
+                using (var ms = new MemoryStream())
+                {
+                    Image.CopyTo(ms);
+                    fileBytes = ms.ToArray();
+                }
+            }
+            return new Park
+            {
+                Name = Name,
+                Description = Description,
+                Year = Year,
+                Image = fileBytes,
+                Longitude = Longitude,
+                Latitude = Latitude,
+                WorkingHours = WorkingHours,
+                CreateDate = DateTime.Now,
+                Safety = Safety,
+            };
+
+        }
+    }
+}
+
