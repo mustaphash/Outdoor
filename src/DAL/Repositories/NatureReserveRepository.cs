b388d4d9d888ffe505668ba8138d8ba392fffd6f
Mustafa Shakir <mshakir9918@gmail.com>
2022-06-23 19:02:47 +0300

    Add GetById Method for place Repositories

--- src/DAL/Repositories/NatureReserveRepository.cs
+++ src/DAL/Repositories/NatureReserveRepository.cs
@@ -1,12 +1,24 @@
 ﻿using Core.Entities;
 using DAL.Repositories.Abstract;
+using Microsoft.EntityFrameworkCore;
 
 namespace DAL.Repositories
 {
     public class NatureReserveRepository : BaseRepository<NatureReserve>, INatureReserveRepository
     {
+        private OutdoorContext _context;
         public NatureReserveRepository(OutdoorContext context) : base(context)
         {
+            _context = context;
         }
+
+#pragma warning disable CS8603
+        public async Task<NatureReserve> GetReserveById(int id)
+        {
+            var natureReserve = await _context.NatureReserves.FirstOrDefaultAsync(n => n.Id == id);
+ 
+            return natureReserve;
+        }
+#pragma warning disable CS8603 
     }
 }
--- src/DAL/Repositories/VillaRepository.cs
+++ src/DAL/Repositories/VillaRepository.cs
@@ -1,12 +1,24 @@
 ﻿using Core.Entities;
 using DAL.Repositories.Abstract;
+using Microsoft.EntityFrameworkCore;
 
 namespace DAL.Repositories
 {
     public class VillaRepository : BaseRepository<Villa>, IVillaRepository
     {
+        private OutdoorContext _context;
         public VillaRepository(OutdoorContext context) : base(context)
         {
+            _context = context;
         }
+
+#pragma warning disable CS8603
+        public async Task<Villa> GetVillaById(int id)
+        {
+            var villa = await _context.Villas.FirstOrDefaultAsync(v=>v.Id == id);
+
+            return villa;
+        }
+#pragma warning disable CS8603
     }
 }
