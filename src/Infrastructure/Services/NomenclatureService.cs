a992c2e3116f4f391dccd98aa5e826b2c3cda1a5
Mustafa Shakir <mshakir9918@gmail.com>
2022-07-25 16:06:21 +0300

    Add Logs to NomenclatureService

--- src/Infrastructure/Services/NomenclatureService.cs
+++ src/Infrastructure/Services/NomenclatureService.cs
@@ -1,39 +1,53 @@
-﻿using DAL;
+﻿using Common;
+using Common.LoggerRecources;
+using DAL;
 using Infrastructure.Models;
 using Infrastructure.Services.Abstract;
+using Microsoft.Extensions.Logging;
 
 namespace Infrastructure.Services
 {
     public class NomenclatureService : INomenclatureService
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger _logger;
 
-        public NomenclatureService(IUnitOfWork unitOfWork)
+        public NomenclatureService(IUnitOfWork unitOfWork, ILogger logger)
         {
             _unitOfWork = unitOfWork;
+            _logger = logger;
         }
 
         public async Task<IList<AnimalModel>> GetAllAnimals()
         {
+            _logger.LogInformation(LogMessages.GettingItem, string.Format(LogMessageResources.GettingItem, nameof(NomenclatureService), nameof(GetAllAnimals)));
+
             var animals = await _unitOfWork.Animals.GetAll();
             var animalsModel = animals.Select(a => new AnimalModel(a)).ToList();
 
+            _logger.LogInformation(LogMessages.GotItem, string.Format(LogMessageResources.GotItem, nameof(NomenclatureService), nameof(GetAllAnimals)));
             return animalsModel;
         }
 
         public async Task<IList<ExtrasModel>> GetAllExtras()
         {
+            _logger.LogInformation(LogMessages.GettingItem, string.Format(LogMessageResources.GettingItem, nameof(NomenclatureService), nameof(GetAllExtras)));
+
             var extras = await _unitOfWork.Extras.GetAll();
             var extrasModel = extras.Select(e => new ExtrasModel(e)).ToList();
 
+            _logger.LogInformation(LogMessages.GotItem, string.Format(LogMessageResources.GotItem, nameof(NomenclatureService), nameof(GetAllExtras)));
             return extrasModel;
         }
 
         public async Task<IList<WaterTypeModel>> GetAllWaterTypes()
         {
+            _logger.LogInformation(LogMessages.GettingItem, string.Format(LogMessageResources.GettingItem, nameof(NomenclatureService), nameof(GetAllWaterTypes)));
+
             var waterTypes = await _unitOfWork.WaterTypes.GetAll();
             var waterTypesModel = waterTypes.Select(e => new WaterTypeModel(e)).ToList();
 
+            _logger.LogInformation(LogMessages.GotItem, string.Format(LogMessageResources.GotItem, nameof(NomenclatureService), nameof(GetAllWaterTypes)));
             return waterTypesModel;
         }
     }
