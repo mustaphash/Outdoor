aea673786221c10939903f10e4008f4a05e8faf2
Mustafa Shakir <mshakir9918@gmail.com>
2022-07-25 14:33:05 +0300

    Create Loggers

---
+++ src/Common/LogMessages.cs
@@ -0,0 +1,14 @@
+﻿namespace Common
+{
+    public class LogMessages
+    {
+        public const int ControllerFound = 1001;
+        public const int ListingItems = 1002;
+        public const int GettingItem = 1003;
+        public const int InsertingItem = 1004;
+
+        public const int ListedItem = 2002;
+        public const int GotItem = 2003;
+        public const int InsertedItem = 2004;
+    }
+}
