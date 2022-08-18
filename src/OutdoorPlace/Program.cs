20e1071698af974f83dfb7df161b34c271aa81b1
Mustafa Shakir <mshakir9918@gmail.com>
2022-08-18 22:17:50 +0300

    Add ValidationModels to Program

--- src/OutdoorPlace/Program.cs
+++ src/OutdoorPlace/Program.cs
@@ -31,11 +31,15 @@ builder.Services.AddScoped<IFountainRepository, FountainRepository>();
 builder.Services.AddScoped<ILandmarkRepository, LandmarkRepository>();
 builder.Services.AddScoped<INatureReserveRepository, NatureReserveRepository>();
 builder.Services.AddScoped<IParkRepository, ParkRepository>();
-builder.Services.AddScoped<IVillaRepository, VillaRepository>();
 builder.Services.AddScoped<IWaterTypeRepository, WaterTypeRepository>();
 builder.Services.AddScoped<IOutdoorRepository, OutdoorRepository>();
+builder.Services.AddScoped<IVillaRepository, VillaRepository>();
 builder.Services.AddScoped<IValidation<CreateLakesModel>, CreateLakesModelValidator>();
 builder.Services.AddScoped<IValidation<CreateFountainsModel>, CreateFountainsModelValidator>();
+builder.Services.AddScoped<IValidation<CreateLandmarksModel>, CreateLandmarkModelValidator>();
+builder.Services.AddScoped<IValidation<CreateNatureReservesModel>, CreateNatureReservesModelValidator>();
+builder.Services.AddScoped<IValidation<CreateParksModel>, CreateParksModelValidator>();
+builder.Services.AddScoped<IValidation<CreateVillasModel>, CreateVillasModelValidator>();
 
 var app = builder.Build();
 
